7a2a29400d0046f608caf6a85e526bdffed01bf3
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-10-24 20:21:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 //your JS code here. If required.
 //
 let button= document.getElementById("enterBtn");
+let change = document.getElementById("status");
 button.addEventListener("click" , onclick);
 
 function onclick(){
 	
 let h1=document.createElement("h1");
-h1.innerText("Entered the Metaverse");
-button.appendChild(h1);
+h1.innerText="Entered the Metaverse";
+change.appendChild(h1);
 	
 }
 
